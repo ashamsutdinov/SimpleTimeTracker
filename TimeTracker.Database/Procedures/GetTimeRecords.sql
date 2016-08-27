abefb7bb2545c0be736be3783c0eb95a015f7911
Aydar Shamsutdinov <aydarrrio@gmail.com>
2016-08-27 21:16:00 +0300

    GetTimeRecords procedure refactored

--- TimeTracker.Database/Procedures/GetTimeRecords.sql
+++ TimeTracker.Database/Procedures/GetTimeRecords.sql
@@ -8,9 +8,7 @@
 	@Total INT OUTPUT
 )
 AS
-BEGIN
-	--page consist of day records, not time records.
-	--each day record consist of any number of time records
+BEGIN	
 	SELECT @Total = COUNT(*) FROM [dbo].[DayRecords] WHERE
 		(@UserId IS NULL OR [UserId] = @UserId) AND
 		(@FromDate IS NULL OR [Date] >= @FromDate) AND
@@ -24,10 +22,42 @@ BEGIN
 		[Name]			NVARCHAR(128),
 		[Date]			DATE,
 		[TotalHours]	INT,
-		[TimeRecordId]	INT
+		[TimeRecordId]	INT,
+		[Hours]			INT
 	)
-	
-	
+
+	DECLARE @DayRecordId INT
+	DECLARE @LoopUserId INT
+	DECLARE @Name NVARCHAR(128)
+	DECLARE @Date DATE
+	DECLARE @TotalHours INT
+
+	DECLARE DayRecordsLoop CURSOR FOR 
+		SELECT [d].[Id], [d].[UserId], [u].[Name], [d].[Date], [d].[TotalHours]
+		FROM [dbo].[DayRecords] [d]
+		INNER JOIN [dbo].[Users] [u] ON [d].[UserId] = [u].[Id]
+		WHERE
+		(@UserId IS NULL OR [d].[UserId] = @UserId) AND
+		(@FromDate IS NULL OR [d].[Date] >= @FromDate) AND
+		(@ToDate IS NULL OR [d].[Date] <= @ToDate) AND 
+		([d].[TotalHours] > 0)
+		ORDER BY [d].[Id] ASC
+		OFFSET (@PageNumber - 1) * @PageSize ROWS
+		FETCH NEXT @PageSize ROWS ONLY
+
+	OPEN DayRecordsLoop
+	FETCH DayRecordsLoop INTO @DayRecordId, @LoopUserId, @Name, @Date, @TotalHours
+
+	WHILE @@FETCH_STATUS = 0
+	BEGIN
+		INSERT INTO #result ([DayRecordId], [UserId], [Name], [Date], [TotalHours], [TimeRecordId], [Hours])
+		SELECT @DayRecordId, @LoopUserId, @Name, @Date, @TotalHours, [t].[Id], [t].[Hours]
+		FROM [dbo].[TimeRecords] [t] WHERE [t].[DayRecordId] = @DayRecordId ORDER BY [t].[Id] ASC		
+		FETCH DayRecordsLoop INTO @DayRecordId, @LoopUserId, @Name, @Date, @TotalHours
+	END
+
+	CLOSE DayRecordsLoop
+	DEALLOCATE DayRecordsLopp	
 	
 	SELECT * FROM #result
 	DROP TABLE #result	
